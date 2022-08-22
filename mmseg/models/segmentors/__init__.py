c1895ae8735a4862fb3d8a9021de7a87bec83539
main-2983 <nguyen.mai@sun-asterisk.com>
2022-08-22 15:01:41 +0700

    Add SunSegmentor

--- mmseg/models/segmentors/__init__.py
+++ mmseg/models/segmentors/__init__.py
@@ -2,5 +2,6 @@
 from .base import BaseSegmentor
 from .cascade_encoder_decoder import CascadeEncoderDecoder
 from .encoder_decoder import EncoderDecoder
+from .sun_segmentor import SunSegmentor
 
-__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder']
+__all__ = ['BaseSegmentor', 'EncoderDecoder', 'CascadeEncoderDecoder', 'SunSegmentor']
