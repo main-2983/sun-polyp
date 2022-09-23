36ee99937d45b92c524732d3f22e967dbc6a0ef7
main-2983 <nguyen.mai@sun-asterisk.com>
2022-09-23 10:05:32 +0700

    LapFormerPPHead_v4

--- mmseg/models/decode_heads/__init__.py
+++ mmseg/models/decode_heads/__init__.py
@@ -41,7 +41,7 @@ from .mlp_slowcatese_head import MLPSLowCatESEHead
 from .mlp_slowcat_la_head import MLPSLowCatLAHead
 from .ssformer_head import SSFormerHead
 from .lapformerpp_head import LAPFormerPPHead, LAPFormerPPHead_v2, LAPFormerPPHead_v3, \
-    ELAPFormerHead
+    ELAPFormerHead, LAPFormerPPHead_v4
 
 __all__ = [
     'FCNHead', 'PSPHead', 'ASPPHead', 'PSAHead', 'NLHead', 'GCHead', 'CCHead',
@@ -56,5 +56,5 @@ __all__ = [
     'MLPSLowCatSEHead', 'MLPSLowCatESEHead', 'MLPSLowCatLAHead',
     'MLP_OSAHead_v2', 'SSFormerHead', 'MLP_OSAHead_v3', 'MLP_OSAHead_v4',
     'MLP_OSAHead_v5', 'LAPFormerPPHead', 'LAPFormerPPHead_v2',
-    'LAPFormerPPHead_v3', 'ELAPFormerHead'
+    'LAPFormerPPHead_v3', 'ELAPFormerHead', 'LAPFormerPPHead_v4'
 ]
--- mmseg/models/decode_heads/lapformerpp_head.py
+++ mmseg/models/decode_heads/lapformerpp_head.py
@@ -5,7 +5,8 @@ from mmcv.cnn import ConvModule
 from mmseg.models.builder import HEADS
 from mmseg.models.decode_heads.decode_head import BaseDecodeHead
 from mmseg.ops import resize
-from mmseg.models.utils import SELayer, CSPSPP, CSPSPPF, SPP, SPPF, C3SPPF
+from mmseg.models.utils import SELayer, CSPSPP, CSPSPPF, SPP, SPPF, C3SPPF, \
+    MultiScaleLocalAttn
 
 
 @HEADS.register_module()
@@ -334,54 +335,6 @@ class LAPFormerPPHead_v3(BaseDecodeHead):
         out = self.cls_seg(out)
 
         return out
-
-
-@HEADS.register_module()
-class LAPFomerPPHead_v4(BaseDecodeHead):
-    def __init__(self,
-                 interpolate_mode='bilinear',
-                 pooling='CSPSPPF',
-                 **kwargs):
-        super(LAPFomerPPHead_v4, self).__init__(
-            input_transform='multiple_select',
-            **kwargs
-        )
-
-        assert pooling in ['SPSPP', 'CSPSPPF', 'SPP', 'SPPF', 'C3SPPF']
-        self.interpolate_mode = interpolate_mode
-
-        num_inputs = len(self.in_channels)
-        assert num_inputs == len(self.in_index)
-
-        self.convs = nn.ModuleList()
-        for i in range(num_inputs):
-            self.convs.append(
-                ConvModule(
-                    in_channels=self.in_channels[i],
-                    out_channels=self.channels,
-                    kernel_size=3,
-                    padding=1,
-                    norm_cfg=self.norm_cfg,
-                    act_cfg=self.act_cfg))
-
-        # feature fusion between adjacent levels
-        self.linear_projections = nn.ModuleList()
-        for i in range(num_inputs - 1):
-            self.linear_projections.append(
-                ConvModule(
-                    in_channels=self.channels * 2,
-                    out_channels=self.channels,
-                    kernel_size=1,
-                    stride=1,
-                    norm_cfg=self.norm_cfg,
-                    act_cfg=self.act_cfg
-                )
-            )
-
-        self.pooling = eval(pooling)(
-            in_channels=self.channels * num_inputs,
-            out_channels=self.channels * num_inputs
-        )
 
 
 @HEADS.register_module()
@@ -724,3 +677,116 @@ class FLAPFormerHead(BaseDecodeHead):
 
         return out
 
+
+@HEADS.register_module()
+class LAPFormerPPHead_v4(BaseDecodeHead):
+    def __init__(self,
+                 interpolate_mode='bilinear',
+                 **kwargs):
+        super(LAPFormerPPHead_v4, self).__init__(
+            input_transform='multiple_select',
+            **kwargs
+        )
+        self.interpolate_mode = interpolate_mode
+        num_inputs = len(self.in_channels)
+
+        assert num_inputs == len(self.in_index)
+
+        self.convs = nn.ModuleList()
+        for i in range(num_inputs):
+            self.convs.append(
+                ConvModule(
+                    in_channels=self.in_channels[i],
+                    out_channels=self.channels,
+                    kernel_size=3,
+                    padding=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg))
+
+        # feature fusion between adjacent levels
+        self.linear_projections = nn.ModuleList()
+        for i in range(num_inputs - 1):
+            self.linear_projections.append(
+                ConvModule(
+                    in_channels=self.channels * 2,
+                    out_channels=self.channels,
+                    kernel_size=1,
+                    stride=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg
+                )
+            )
+
+        self.multi_scale = MultiScaleLocalAttn(
+            channels=self.channels * (num_inputs - 1)
+        )
+
+        self.se_module = SELayer(
+            channels=self.channels * (num_inputs - 1)
+        )
+        self.fusion_conv = ConvModule(
+            in_channels=self.channels * (num_inputs - 1),
+            out_channels=self.channels,
+            kernel_size=1,
+            norm_cfg=self.norm_cfg)
+
+    def forward(self, inputs):
+        # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
+        inputs = self._transform_inputs(inputs)
+        _inputs = [] # 1/4, 1/8, 1/8, 1/8
+        for idx in range(len(inputs)):
+            x = inputs[idx]
+            conv = self.convs[idx]
+            feat = conv(x)
+            if idx > 0:
+                feat = resize(
+                    input=feat,
+                    size=inputs[1].shape[2:],
+                    mode=self.interpolate_mode,
+                    align_corners=self.align_corners
+                )
+            _inputs.append(feat)
+
+        # progressive feature fusion
+        _out = torch.empty(
+            _inputs[1].shape
+        )
+        outs = [_inputs[-1]]
+        for idx in range(len(_inputs) - 1, 0, -1):
+            linear_prj = self.linear_projections[idx - 1]
+            # cat first 2 from _inputs
+            if idx == len(_inputs) - 1:
+                x1 = _inputs[idx]
+                x2 = _inputs[idx - 1]
+            # if not first 2 then cat from prev outs and _inputs
+            else:
+                x1 = _out
+                x2 = _inputs[idx - 1]
+            if idx == 1:
+                x1 = resize(
+                    input=x1,
+                    size=x2.shape[2:],
+                    mode=self.interpolate_mode,
+                    align_corners=self.align_corners
+                )
+            x = torch.cat([x1, x2], dim=1)
+            _out = linear_prj(x)
+            outs.append(_out)
+
+        out = torch.cat(outs[:3], dim=1)
+        out = self.multi_scale(out)
+        out = self.se_module(out)
+        out = self.fusion_conv(out)
+        out = resize(
+            input=out,
+            size=_inputs[0].shape[2:],
+            mode=self.interpolate_mode,
+            align_corners=self.align_corners
+        )
+
+        # perform identity mapping
+        out = outs[-1] + out
+
+        out = self.cls_seg(out)
+
+        return out
