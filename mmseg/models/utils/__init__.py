23a84f4af2ab93d413272642398b59cfff0dc3a9
main-2983 <nguyen.mai@sun-asterisk.com>
2022-09-23 08:15:52 +0700

    2 Conv Attn: LKA and MSCA

--- mmseg/models/utils/__init__.py
+++ mmseg/models/utils/__init__.py
@@ -12,11 +12,13 @@ from .weighted_VA import wVisualAttention
 from .layer_attention import LayerAttention, EfficientLayerAttn
 from .reversed_attention import ReversedAttention
 from .spp import SPP, CSPSPP, SPPF, CSPSPPF, C3SPPF
+from .visual_attention import LargeKernelAttn, MultiScaleConvAttn
 
 __all__ = [
     'ResLayer', 'SelfAttentionBlock', 'make_divisible', 'InvertedResidual',
     'UpConvBlock', 'InvertedResidualV3', 'SELayer', 'PatchEmbed',
     'nchw_to_nlc', 'nlc_to_nchw', 'nchw2nlc2nchw', 'nlc2nchw2nlc',
     'wVisualAttention', 'LayerAttention', 'EfficientSELayer',
-    'EfficientLayerAttn', 'GeSELayer', 'SPP', 'CSPSPP', 'SPPF', 'CSPSPPF', 'C3SPPF'
+    'EfficientLayerAttn', 'GeSELayer', 'SPP', 'CSPSPP', 'SPPF', 'CSPSPPF', 'C3SPPF',
+    'LargeKernelAttn', 'MultiScaleConvAttn'
 ]
--- mmseg/models/utils/visual_attention.py
+++ mmseg/models/utils/visual_attention.py
@@ -3,5 +3,109 @@ import torch.nn as nn
 from mmcv.cnn import ConvModule
 
 
-class LargeKernelAttention(nn.Module):
-    pass
+""" A set of Convolutional Attention from VAN's authors """
+class LargeKernelAttn(nn.Module):
+    def __init__(self,
+                 channels):
+        super(LargeKernelAttn, self).__init__()
+        self.dwconv = nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=5,
+            padding=2,
+            groups=channels
+        )
+        self.dwdconv = nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=7,
+            padding=9,
+            groups=channels,
+            dilation=3
+        )
+        self.pwconv = nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=1
+        )
+
+    def forward(self, x):
+        weight = self.pwconv(self.dwdconv(self.dwconv(x)))
+
+        return x * weight
+
+
+class MultiScaleConvAttn(nn.Module):
+    def __init__(self,
+                 channels):
+        super(MultiScaleConvAttn, self).__init__()
+        self.dwconv = nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=5,
+            padding=2,
+            groups=channels
+        )
+        self.scale_7 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(1, 7),
+                padding=(0, 3),
+                groups=channels
+            ),
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(7, 1),
+                padding=(3, 0),
+                groups=channels
+            )
+        )
+        self.scale_11 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(1, 11),
+                padding=(0, 5),
+                groups=channels
+            ),
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(11, 1),
+                padding=(5, 0),
+                groups=channels
+            )
+        )
+        self.scale_21 = nn.Sequential(
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(1, 21),
+                padding=(0, 10),
+                groups=channels
+            ),
+            nn.Conv2d(
+                in_channels=channels,
+                out_channels=channels,
+                kernel_size=(21, 1),
+                padding=(10, 0),
+                groups=channels
+            )
+        )
+        self.pwconv = nn.Conv2d(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=1
+        )
+
+    def forward(self, x):
+        base_weight = self.dwconv(x)
+        weight1 = self.scale_7(base_weight)
+        weight2 = self.scale_11(base_weight)
+        weight3 = self.scale_21(base_weight)
+        weight = base_weight + weight1 + weight2 + weight3
+        weight = self.pwconv(weight)
+
+        return x * weight
