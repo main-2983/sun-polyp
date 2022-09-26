af338dd7939847056424b66896eeabdcd23f16f0
main-2983 <nguyen.mai@sun-asterisk.com>
2022-09-26 09:53:15 +0700

    v4

--- mmseg/models/decode_heads/lapformerpp_head.py
+++ mmseg/models/decode_heads/lapformerpp_head.py
@@ -436,11 +436,9 @@ class ELAPFormerHead(BaseDecodeHead):
 class ELAPFormerPPHead_v2(BaseDecodeHead):
     def __init__(self,
                  interpolate_mode='bilinear',
-                 act_cfg=dict(type='GELU'),
                  **kwargs):
         super(ELAPFormerPPHead_v2, self).__init__(
             input_transform='multiple_select',
-            act_cfg=act_cfg,
             **kwargs
         )
         self.interpolate_mode = interpolate_mode
@@ -526,141 +524,6 @@ class ELAPFormerPPHead_v2(BaseDecodeHead):
             outs.append(_out)
 
         out = torch.cat(outs[:3], dim=1)
-        out = self.se_module(out)
-        out = self.fusion_conv(out)
-        out = resize(
-            input=out,
-            size=_inputs[0].shape[2:],
-            mode=self.interpolate_mode,
-            align_corners=self.align_corners
-        )
-
-        # perform identity mapping
-        out = outs[-1] + out
-
-        out = self.cls_seg(out)
-
-        return out
-
-
-@HEADS.register_module()
-class FLAPFormerHead(BaseDecodeHead):
-    def __init__(self,
-                 interpolate_mode='bilinear',
-                 **kwargs):
-        super().__init__(input_transform='multiple_select', **kwargs)
-
-        self.interpolate_mode = interpolate_mode
-        num_inputs = len(self.in_channels)
-
-        assert num_inputs == len(self.in_index)
-
-        # reduce channel dims and local emphasis
-        self.convs = nn.ModuleList()
-        for i in range(num_inputs):
-            self.convs.append(
-                ConvModule(
-                    in_channels=self.in_channels[i],
-                    out_channels=self.channels,
-                    kernel_size=3,
-                    padding=1,
-                    norm_cfg=self.norm_cfg,
-                    act_cfg=self.act_cfg))
-
-        # feature fusion between adjacent levels
-        self.linear_projections = nn.ModuleList()
-        for i in range(num_inputs - 1):
-            self.linear_projections.append(
-                ConvModule(
-                    in_channels=self.channels * 2,
-                    out_channels=self.channels,
-                    kernel_size=1,
-                    stride=1,
-                    norm_cfg=self.norm_cfg,
-                    act_cfg=self.act_cfg
-                )
-            )
-
-        self.highres_path = nn.ModuleList([
-            nn.Sequential(
-                nn.Conv2d(
-                    in_channels=self.channels,
-                    out_channels=self.channels,
-                    kernel_size=1
-                ),
-                nn.Conv2d(
-                    in_channels=self.channels,
-                    out_channels=self.channels,
-                    kernel_size=2,
-                    stride=2
-                )
-            ),
-            nn.Conv2d(
-                in_channels=self.channels,
-                out_channels=self.channels,
-                kernel_size=1
-            )
-        ])
-
-        self.se_module = SELayer(
-            channels=self.channels * num_inputs
-        )
-        self.fusion_conv = ConvModule(
-            in_channels=self.channels * num_inputs,
-            out_channels=self.channels,
-            kernel_size=1,
-            norm_cfg=self.norm_cfg)
-
-    def forward(self, inputs):
-        # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
-        inputs = self._transform_inputs(inputs)
-        _inputs = [] # 1/4, 1/8, 1/8, 1/8
-        for idx in range(len(inputs)):
-            x = inputs[idx]
-            conv = self.convs[idx]
-            feat = conv(x)
-            if idx > 0:
-                feat = resize(
-                    input=feat,
-                    size=inputs[1].shape[2:],
-                    mode=self.interpolate_mode,
-                    align_corners=self.align_corners
-                )
-            _inputs.append(feat)
-
-        # progressive feature fusion
-        _out = torch.empty(
-            _inputs[1].shape
-        )
-        outs = [_inputs[-1]] # 1/8, 1/8, 1/8, (1/8, 1/4)
-        for idx in range(len(_inputs) - 1, 0, -1):
-            linear_prj = self.linear_projections[idx - 1]
-            # cat first 2 from _inputs
-            if idx == len(_inputs) - 1:
-                x1 = _inputs[idx]
-                x2 = _inputs[idx - 1]
-            # if not first 2 then cat from prev outs and _inputs
-            else:
-                x1 = _out
-                x2 = _inputs[idx - 1]
-            if idx == 1:
-                x1 = resize(
-                    input=x1,
-                    size=x2.shape[2:],
-                    mode=self.interpolate_mode,
-                    align_corners=self.align_corners
-                )
-            x = torch.cat([x1, x2], dim=1)
-            _out = linear_prj(x)
-            # CSP-ized highest res featmaps
-            if idx == 1:
-                _out1 = self.highres_path[0](_out)
-                _out2 = self.highres_path[1](_out)
-                outs.extend([_out1, _out2])
-            else:
-                outs.append(_out)
-
-        out = torch.cat(outs[:4], dim=1)
         out = self.se_module(out)
         out = self.fusion_conv(out)
         out = resize(
