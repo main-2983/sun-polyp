e89ab8086e420137c2dbbd13f03d1c96b9eee7e1
main-2983 <nguyen.mai@sun-asterisk.com>
2022-10-17 14:36:42 +0700

    currently working

--- mcode/config.py
+++ mcode/config.py
@@ -22,12 +22,12 @@ wandb_dir = "./wandb"
 
 seed = 2022
 device = select_device("cuda:0" if torch.cuda.is_available() else 'cpu')
-num_workers = 4
+num_workers = 0
 
-train_images = glob.glob('../Dataset/polyp/TrainDataset/images/*')
-train_masks = glob.glob('../Dataset/polyp/TrainDataset/masks/*')
+train_images = glob.glob('Dataset/TrainDataset/image/*')
+train_masks = glob.glob('Dataset/TrainDataset/mask/*')
 
-test_folder = "../Dataset/polyp/TestDataset"
+test_folder = "Dataset/TestDataset"
 test_images = glob.glob(f'{test_folder}/*/images/*')
 test_masks = glob.glob(f'{test_folder}/*/masks/*')
 
@@ -35,7 +35,7 @@ save_path = "runs/test"
 
 image_size = 352
 
-bs = 16
+bs = 1
 bs_val = 2
 grad_accumulate_rate = 1
 
@@ -59,12 +59,6 @@ loss_weights = [0.8, 0.2]
 train_transform = A.Compose([
     A.HorizontalFlip(p=0.5),
     A.VerticalFlip(p=0.5),
-    A.RandomGamma (gamma_limit=(50, 150), eps=None, always_apply=False, p=0.5),
-    A.RandomBrightness(p=0.3),
-    A.RGBShift(p=0.3, r_shift_limit=5, g_shift_limit=5, b_shift_limit=5),
-    A.OneOf([A.Blur(), A.GaussianBlur(), A.GlassBlur(), A.MotionBlur(), A.GaussNoise(), A.Sharpen(), A.MedianBlur(), A.MultiplicativeNoise()]),
-    A.Cutout(p=0.3, max_h_size=25, max_w_size=25, fill_value=255),
-    A.ShiftScaleRotate(p=0.3, border_mode=cv2.BORDER_CONSTANT, shift_limit=0.15, scale_limit=0.11),
     A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     ToTensorV2(),
 ])
@@ -74,35 +68,32 @@ val_transform = A.Compose([
     ToTensorV2(),
 ])
 
-pretrained = "/mnt/sdd/nguyen.van.quan/BKAI-kaggle/pretrained/mit_b1_mmseg.pth"
+pretrained = 'https://github.com/rwightman/pytorch-image-models/releases/download/v0.1-res2net/res2net50_26w_4s-06e79181.pth'
 model_cfg = dict(
     type='SunSegmentor',
     backbone=dict(
-        type='MixVisionTransformer',
-        in_channels=3,
-        embed_dims=64,
-        num_stages=4,
-        num_layers=[2, 2, 2, 2],
-        num_heads=[1, 2, 5, 8],
-        patch_sizes=[7, 3, 3, 3],
-        sr_ratios=[8, 4, 2, 1],
-        out_indices=(0, 1, 2, 3),
-        mlp_ratio=4,
-        qkv_bias=True,
-        drop_rate=0.0,
-        attn_drop_rate=0.0,
-        drop_path_rate=0.1,
-        pretrained=None),
+        type='TIMMBackbone',
+        model_name='res2net50_26w_4s',
+        out_indices=[1, 2, 3, 4],
+        init_cfg=dict(
+            type='Pretrained',
+            checkpoint=pretrained
+        )
+    ),
+    neck=dict(
+        type='FPN',
+        in_channels=[256, 512, 1024, 2048],
+        out_channels=256,
+        num_outs=4
+    ),
     decode_head=dict(
-        type='SSFormerHead',
-        in_channels=[64, 128, 320, 512],
-        in_index=[0, 1, 2, 3],
-        channels=256,
-        dropout_ratio=0.1,
+        type='FCNHead',
         num_classes=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        align_corners=False,
-        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0))
+        input_transform='resize_concat',
+        in_index=[0, 1, 2, 3],
+        in_channels=[256, 256, 256, 256],
+        channels=256
+    )
 )
 
 # ===============================================================================
