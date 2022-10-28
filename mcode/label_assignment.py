82e84762f520095833148950ede083c2d2c8e0bb
main-2983 <nguyen.mai@sun-asterisk.com>
2022-10-28 09:09:19 +0700

    Strat 5

--- mcode/label_assignment.py
+++ mcode/label_assignment.py
@@ -74,3 +74,23 @@ def strategy_4(preds: list[torch.Tensor], target: torch.Tensor=None, num_outs=3)
         aux_targets.append(pred)
     targets.extend(aux_targets)
     return targets
+
+
+@torch.no_grad()
+def strategy_5(preds: list[torch.Tensor], target: torch.Tensor=None, num_outs=3,
+               cur_ep=None, total_eps=20, frac=0.8):
+    ep_to_change = int(total_eps * frac)
+    if cur_ep <= ep_to_change:
+        targets = [target] * len(preds)
+        return targets
+    else:
+        targets = []
+        targets.append(target)
+        aux_targets = []
+        for i in range(num_outs):
+            pred = preds[0]
+            pred = pred.sigmoid()
+            pred = (pred > 0.5).float()
+            aux_targets.append(pred)
+        targets.extend(aux_targets)
+        return targets
