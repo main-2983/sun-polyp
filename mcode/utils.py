b04b7a20089073bc7267a8194f6f39d1e0dea39f
main-2983 <nguyen.mai@sun-asterisk.com>
2022-11-01 10:04:02 +0700

    Add utils for pranet_train, add label assignment base

--- mcode/utils.py
+++ mcode/utils.py
@@ -11,6 +11,25 @@ from mmcv.cnn import get_model_complexity_info
 from pytorch_lightning import seed_everything
 
 
+def adjust_lr(optimizer, init_lr, epoch, decay_rate=0.1, decay_epoch=30):
+    decay = decay_rate ** (epoch // decay_epoch)
+    for param_group in optimizer.param_groups:
+        param_group['lr'] *= decay
+
+
+def clip_gradient(optimizer, grad_clip):
+    """
+    For calibrating misalignment gradient via cliping gradient technique
+    :param optimizer:
+    :param grad_clip:
+    :return:
+    """
+    for group in optimizer.param_groups:
+        for param in group['params']:
+            if param.grad is not None:
+                param.grad.data.clamp_(-grad_clip, grad_clip)
+
+
 def set_seed_everything(seed: int):
     random.seed(seed)
     os.environ['PYTHONHASHSEED'] = str(seed)
