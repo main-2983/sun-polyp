ca48fb238e6f33b3ec981c9fb5388e8f360da0c2
main-2983 <nguyen.mai@sun-asterisk.com>
2022-11-02 09:51:36 +0700

    Add multi_apply func

--- mcode/utils.py
+++ mcode/utils.py
@@ -3,12 +3,35 @@ import os
 import numpy as np
 import logging
 import platform
+from functools import partial
 
 import torch
 
 from mmcv.cnn import get_model_complexity_info
 
 from pytorch_lightning import seed_everything
+
+
+def multi_apply(func, *args, **kwargs):
+    """Apply function to a list of arguments.
+
+    Note:
+        This function applies the ``func`` to multiple inputs and
+        map the multiple outputs of the ``func`` into different
+        list. Each list contains the same type of outputs corresponding
+        to different inputs.
+
+    Args:
+        func (Function): A function that will be applied to a list of
+            arguments
+
+    Returns:
+        tuple(list): A tuple containing multiple list, each list contains \
+            a kind of returned results by the function
+    """
+    pfunc = partial(func, **kwargs) if kwargs else func
+    map_results = map(pfunc, *args)
+    return tuple(map(list, zip(*map_results)))
 
 
 def adjust_lr(optimizer, init_lr, epoch, decay_rate=0.1, decay_epoch=30):
