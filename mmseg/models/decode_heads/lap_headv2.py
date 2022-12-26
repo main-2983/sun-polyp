956deafc8466a836086217548c496a97d6fd9932
main-2983 <nguyen.mai@sun-asterisk.com>
2022-12-26 15:08:48 +0700

    Change LAPHeadv2(1)

--- mmseg/models/decode_heads/lap_headv2.py
+++ mmseg/models/decode_heads/lap_headv2.py
@@ -59,41 +59,58 @@ class LAPHead_v2_1(BaseDecodeHead):
     def forward(self, inputs):
         # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
         inputs = self._transform_inputs(inputs)
-        _inputs = []
+        _inputs = [] # 1/4, 1/8, 1/16, 1/32
         for idx in range(len(inputs)):
             x = inputs[idx]
             conv = self.convs[idx]
-            _inputs.append(
-                resize(
-                    input=conv(x),
-                    size=inputs[0].shape[2:],
-                    mode=self.interpolate_mode,
-                    align_corners=self.align_corners))
+            _inputs.append(conv(x))
 
         # slow concatenate
-        _out = torch.empty(
-            _inputs[0].shape
-        )
-        outs = [_inputs[-1]]
+        outs = [resize(_inputs[-1],
+                       size=inputs[1].shape[2:],
+                       mode=self.interpolate_mode,
+                       align_corners=self.align_corners)] # 1/8, 1/8, 1/8, 1/4
+        # reversed order
         for idx in range(len(_inputs) - 1, 0, -1):
             linear_prj = self.linear_projections[idx - 1]
             # cat first 2 from _inputs
             if idx == len(_inputs) - 1:
-                x1 = _inputs[idx]
-                x2 = _inputs[idx - 1]
-            # if not first 2 then cat from prev outs and _inputs
+                x1 = _inputs[idx] # scale_i
+                x2 = _inputs[idx - 1] # scale_i * 2
+            # if not first 2 then cat from prev out (_out) and _inputs
             else:
-                x1 = _out
-                x2 = _inputs[idx - 1]
+                x1 = _out # scale_i
+                x2 = _inputs[idx - 1] # scale_i * 2
+            if idx == 1:
+                # resize to 1/4
+                x1 = resize(x1,
+                            size=inputs[0].shape[2:],
+                            mode=self.interpolate_mode,
+                            align_corners=self.align_corners)
+            else:
+                # resize to 1/8
+                x1 = resize(x1,
+                            size=inputs[1].shape[2:],
+                            mode=self.interpolate_mode,
+                            align_corners=self.align_corners)
+                x2 = resize(x2,
+                            size=inputs[1].shape[2:],
+                            mode=self.interpolate_mode,
+                            align_corners=self.align_corners)
             x = torch.cat([x1, x2], dim=1)
             _out = linear_prj(x)
             outs.append(_out)
 
+        # cat all 1/8
         out = torch.cat(outs[:-1], dim=1)
         out = self.se_module(out)
         out = self.fusion_conv(out)
 
         # perform identity mapping
+        out = resize(input=out,
+                     size=inputs[0].shape[2:],
+                     mode=self.interpolate_mode,
+                     align_corners=self.align_corners)
         out = outs[-1] + out
 
         out = self.cls_seg(out)
