c8b9abd3ab2f83577fed663f352c3f7981b34c18
main-2983 <nguyen.mai@sun-asterisk.com>
2023-01-03 10:05:14 +0700

    v2(4)

--- mmseg/models/decode_heads/lap_headv2.py
+++ mmseg/models/decode_heads/lap_headv2.py
@@ -397,3 +397,145 @@ class LAPHead_v2_3(BaseDecodeHead):
         out = self.cls_seg(out)
 
         return out
+
+
+# Add PPM, remove 1/4 in concatenation ans PFF, add in later
+@HEADS.register_module()
+class LAPHead_v2_4(BaseDecodeHead):
+    def __init__(self,
+                 interpolate_mode='bilinear',
+                 pool_scales=(1, 2, 3, 6),
+                 ppm_chans=128,
+                 bottleneck_ksize=1,
+                 **kwargs):
+        super().__init__(input_transform='multiple_select', **kwargs)
+
+        self.interpolate_mode = interpolate_mode
+        num_inputs = len(self.in_channels)
+
+        assert num_inputs == len(self.in_index)
+
+        # ppm module and bottleneck
+        self.ppm = PPM(
+            in_channels=self.in_channels[-1],
+            pool_scales=pool_scales,
+            channels=ppm_chans,
+            conv_cfg=None,
+            norm_cfg=None,
+            act_cfg=self.act_cfg,
+            align_corners=self.align_corners
+        )
+        self.bottleneck = ConvModule(
+            in_channels=self.in_channels[-1] + len(pool_scales) * ppm_chans,
+            out_channels=self.channels,
+            kernel_size=bottleneck_ksize,
+            padding=bottleneck_ksize // 2,
+            stride=1,
+            norm_cfg=self.norm_cfg
+        )
+
+        # feature refinement - 3 modules
+        self.convs = nn.ModuleList()
+        for i in range(num_inputs - 1):
+            self.convs.append(
+                ConvModule(
+                    in_channels=self.in_channels[i],
+                    out_channels=self.channels,
+                    kernel_size=3,
+                    padding=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg))
+
+        # feature fusion between adjacent levels, exclude 1/4
+        self.linear_projections = nn.ModuleList()
+        for i in range(num_inputs - 2):
+            self.linear_projections.append(
+                ConvModule(
+                    in_channels=self.channels * 2,
+                    out_channels=self.channels,
+                    kernel_size=1,
+                    stride=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg
+                )
+            )
+
+        self.se_module = SELayer(
+            channels=self.channels * (num_inputs - 1)
+        )
+        self.fusion_conv = ConvModule(
+            in_channels=self.channels * (num_inputs - 1),
+            out_channels=self.channels,
+            kernel_size=1,
+            norm_cfg=self.norm_cfg)
+
+    def forward_ppm(self, inputs):
+        # apply PPM on 1/32
+        ppm_out = self.ppm(inputs[-1])
+        ppm_out.append(inputs[-1])
+        ppm_out = torch.cat(ppm_out, dim=1)
+        ppm_out = self.bottleneck(ppm_out)
+        return ppm_out
+
+    def forward(self, inputs):
+        # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
+        inputs = self._transform_inputs(inputs)
+        # forward ppm, resize ppm_out to 1/8
+        inputs = list(inputs)
+        inputs[-1] = self.forward_ppm(inputs)
+        inputs[-1] = resize(input=inputs[-1],
+                            size=inputs[1].shape[2:],
+                            mode=self.interpolate_mode,
+                            align_corners=self.align_corners)
+
+        # change inputs from 1/4, 1/8, 1/16, 1/32 -> 1/4, 1/8, 1/8, 1/8
+        for idx in range(len(inputs) - 1):
+            x = inputs[idx]
+            conv = self.convs[idx]
+            feat = conv(x)
+            if idx > 0:
+                feat = resize(
+                    input=feat,
+                    size=inputs[1].shape[2:],
+                    mode=self.interpolate_mode,
+                    align_corners=self.align_corners
+                )
+            inputs[idx] = feat
+        _inputs = inputs[1:] # 1/8, 1/8, 1/8
+
+        # progressive feature fusion
+        # placeholder
+        _out = torch.empty(
+            _inputs[1].shape
+        )
+        outs = [_inputs[-1]] # 1/8, 1/8, 1/8
+        for idx in range(len(_inputs) - 1, 0, -1):
+            linear_prj = self.linear_projections[idx - 1]
+            # cat first 2 from _inputs
+            if idx == len(_inputs) - 1:
+                x1 = _inputs[idx]
+                x2 = _inputs[idx - 1]
+            # if not first 2 then cat from prev outs and _inputs
+            else:
+                x1 = _out
+                x2 = _inputs[idx - 1]
+            x = torch.cat([x1, x2], dim=1)
+            _out = linear_prj(x)
+            outs.append(_out)
+
+        out = torch.cat(outs, dim=1)
+        out = self.se_module(out)
+        out = self.fusion_conv(out)
+        out = resize(
+            input=out,
+            size=_inputs[0].shape[2:],
+            mode=self.interpolate_mode,
+            align_corners=self.align_corners
+        )
+
+        # perform identity mapping
+        out = outs[-1] + out
+
+        out = self.cls_seg(out)
+
+        return out
