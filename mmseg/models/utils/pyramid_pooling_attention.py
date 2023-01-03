755d8baecc5b414ded321fa0fa43446b9d79a61e
main-2983 <nguyen.mai@sun-asterisk.com>
2023-01-03 16:25:02 +0700

    Pyramid Pooling Attention

---
+++ mmseg/models/utils/pyramid_pooling_attention.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+
+from mmcv.cnn import ConvModule
+
+from mmseg.ops import resize
+
+class PyramidPoolingAttention(nn.Module):
+    """
+    This module use PPM (Pyramid Pooling Module) as an Attention module
+    The attention weight is the combination of different pool scale
+    """
+    def __init__(self,
+                 channels,
+                 act_cfg=None,
+                 pool_scales=(1, 2, 3, 6)):
+        super(PyramidPoolingAttention, self).__init__()
+        self.pools = nn.ModuleList()
+        for pool_scale in pool_scales:
+            self.pools.append(
+                nn.AdaptiveAvgPool2d(pool_scale)
+            )
+        self.bottleneck = ConvModule(
+            in_channels=channels,
+            out_channels=channels,
+            kernel_size=1,
+            act_cfg=act_cfg
+        )
+
+    def forward(self, x):
+        ppm_outs = []
+        for pool in self.pools:
+            out = pool(x)
+            upsampled_out = resize(
+                input=out,
+                size=x.shape[2:],
+                mode='bilinear',
+                align_corners=False
+            )
+            ppm_outs.append(upsampled_out)
+        attn_weight = sum(ppm_outs)
+        attn_weight = self.bottleneck(attn_weight)
+        return x * attn_weight
