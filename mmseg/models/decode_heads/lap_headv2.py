10cbe4db5855c68621b85c1308e83b693d7b2b94
main-2983 <nguyen.mai@sun-asterisk.com>
2023-01-04 13:56:04 +0700

    v2(7) - StripPoolingAttention

--- mmseg/models/decode_heads/lap_headv2.py
+++ mmseg/models/decode_heads/lap_headv2.py
@@ -7,6 +7,7 @@ from mmseg.models.decode_heads.decode_head import BaseDecodeHead
 from mmseg.ops import resize
 from mmseg.models.utils import SELayer
 from mmseg.models.utils.pyramid_pooling_attention import PyramidPoolingAttention
+from mmseg.models.utils.strip_attention import StripPoolingAttention
 from .psp_head import PPM
 
 
@@ -761,6 +762,133 @@ class LAPHead_v2_6(BaseDecodeHead):
         # forward ppm
         inputs = list(inputs)
         inputs[-1] = self.forward_ppm(inputs)
+        inputs[-1] = resize(input=inputs[-1],
+                            size=inputs[0].shape[2:],
+                            mode=self.interpolate_mode,
+                            align_corners=self.align_corners)
+
+        _inputs = [inputs[-1]]
+        for idx in range(len(inputs) - 1):
+            x = inputs[idx]
+            conv = self.convs[idx]
+            _inputs.append(
+                resize(
+                    input=conv(x),
+                    size=inputs[0].shape[2:],
+                    mode=self.interpolate_mode,
+                    align_corners=self.align_corners))
+
+        # slow concatenate
+        _out = torch.empty(
+            _inputs[0].shape
+        )
+        outs = [_inputs[-1]]
+        for idx in range(len(_inputs) - 1, 0, -1):
+            linear_prj = self.linear_projections[idx - 1]
+            # cat first 2 from _inputs
+            if idx == len(_inputs) - 1:
+                x1 = _inputs[idx]
+                x2 = _inputs[idx - 1]
+            # if not first 2 then cat from prev outs and _inputs
+            else:
+                x1 = _out
+                x2 = _inputs[idx - 1]
+            x = torch.cat([x1, x2], dim=1)
+            _out = linear_prj(x)
+            outs.append(_out)
+
+        out = torch.cat(outs, dim=1)
+        out = self.se_module(out)
+        out = self.fusion_conv(out)
+
+        # perform identity mapping
+        out = outs[-1] + out
+
+        out = self.cls_seg(out)
+
+        return out
+
+
+# Add StripPoolingAttention to last scale
+@HEADS.register_module()
+class LAPHead_v2_7(BaseDecodeHead):
+    def __init__(self,
+                 interpolate_mode='bilinear',
+                 mid_act=dict(
+                     type='ReLU'
+                 ),
+                 act_cfg=dict(
+                     type='Sigmoid'
+                 ),
+                 **kwargs):
+        super().__init__(input_transform='multiple_select', **kwargs)
+
+        self.interpolate_mode = interpolate_mode
+        num_inputs = len(self.in_channels)
+
+        assert num_inputs == len(self.in_index)
+
+        # PPM Attention
+        self.strip_attn = StripPoolingAttention(
+            channels=self.in_channels[-1],
+            mid_act=mid_act,
+            act_cfg=act_cfg
+        )
+        self.bottleneck = ConvModule(
+            in_channels=self.in_channels[-1],
+            out_channels=self.channels,
+            kernel_size=1,
+            act_cfg=self.act_cfg,
+            norm_cfg=self.norm_cfg
+        )
+
+        # reduce channel dims and local emphasis
+        self.convs = nn.ModuleList()
+        for i in range(num_inputs - 1):
+            self.convs.append(
+                ConvModule(
+                    in_channels=self.in_channels[i],
+                    out_channels=self.channels,
+                    kernel_size=3,
+                    padding=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg))
+
+        # feature fusion between adjacent levels
+        self.linear_projections = nn.ModuleList()
+        for i in range(num_inputs - 1):
+            self.linear_projections.append(
+                ConvModule(
+                    in_channels=self.channels * 2,
+                    out_channels=self.channels,
+                    kernel_size=1,
+                    stride=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg
+                )
+            )
+
+        self.se_module = SELayer(
+            channels=self.channels * num_inputs
+        )
+        self.fusion_conv = ConvModule(
+            in_channels=self.channels * num_inputs,
+            out_channels=self.channels,
+            kernel_size=1,
+            norm_cfg=self.norm_cfg)
+
+    def forward_strip(self, inputs):
+        # apply StripPoolingAttention on 1/32
+        ppm_out = self.strip_attn(inputs[-1])
+        ppm_out = self.bottleneck(ppm_out)
+        return ppm_out
+
+    def forward(self, inputs):
+        # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
+        inputs = self._transform_inputs(inputs)
+        # forward ppm
+        inputs = list(inputs)
+        inputs[-1] = self.forward_strip(inputs)
         inputs[-1] = resize(input=inputs[-1],
                             size=inputs[0].shape[2:],
                             mode=self.interpolate_mode,
