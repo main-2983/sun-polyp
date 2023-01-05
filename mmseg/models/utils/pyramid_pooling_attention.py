70d0b69985fc74ed42b80d7bc28369ed4b553e58
main-2983 <nguyen.mai@sun-asterisk.com>
2023-01-05 08:48:04 +0700

    PPA version 2 - Drop Bottleneck

--- mmseg/models/utils/pyramid_pooling_attention.py
+++ mmseg/models/utils/pyramid_pooling_attention.py
@@ -40,3 +40,34 @@ class PyramidPoolingAttention(nn.Module):
         attn_weight = sum(ppm_outs)
         attn_weight = self.bottleneck(attn_weight)
         return x * attn_weight
+
+
+class PyramidPoolingAttentionv2(nn.Module):
+    """
+    This module use PPM (Pyramid Pooling Module) as an Attention module
+    The attention weight is the combination of different pool scale
+    The difference between this and v1 is: v2 doesn't include a bottleneck
+    """
+    def __init__(self,
+                 channels,
+                 pool_scales=(1, 2, 3, 6)):
+        super(PyramidPoolingAttentionv2, self).__init__()
+        self.pools = nn.ModuleList()
+        for pool_scale in pool_scales:
+            self.pools.append(
+                nn.AdaptiveAvgPool2d(pool_scale)
+            )
+
+    def forward(self, x):
+        ppm_outs = []
+        for pool in self.pools:
+            out = pool(x)
+            upsampled_out = resize(
+                input=out,
+                size=x.shape[2:],
+                mode='bilinear',
+                align_corners=False
+            )
+            ppm_outs.append(upsampled_out)
+        attn_weight = sum(ppm_outs)
+        return x * attn_weight
