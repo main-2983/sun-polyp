01280724d9c23eeeb0fb1d09a029c95c7f86942d
huytq-1247 <trinh.quang.huy@sun-asterisk.com>
2023-02-21 07:42:59 +0000

    config sam

--- mcode/config.py
+++ mcode/config.py
@@ -1,77 +1,93 @@
 import glob
-import torch.nn as nn
+
 import torch
 import segmentation_models_pytorch as smp
 import albumentations as A
 from albumentations.pytorch import ToTensorV2
 import cv2
-import os
+
 from .utils import select_device
 from .metrics import AverageMeter
 from .label_assignment import *
 
-name_model = "LAPFormerHead_new_13"
-name_wandb = "LAPFormerHead_new_13_aux_loss"
+
+name_model = "LAPFormerHead"
+name_wandb = "LAPFormerHead_sam"
 # config
 # ===============================================================================
-use_wandb = True
+use_wandb = False
 wandb_key = "1424c55fa73c0d5684ab0210260f866920bb498d"
 wandb_project = "Polyp-Research"
 wandb_entity = "ssl-online"
-wandb_name = '4'
+wandb_name = '0'
 wandb_group = name_wandb
-# wandb_dir = "~/wandb"
+wandb_dir = "~/wandb"
 
-seed = 202
-list_seed = [2023, 2022, 202, 20, 22, 23, 2, 32, 30, 3]
-device = select_device("cuda:1" if torch.cuda.is_available() else 'cpu')
-# device = "cuda:1"
+# device config
+# ------------------------------------------------
+seed = 2022
+device = select_device("cuda:0" if torch.cuda.is_available() else 'cpu')
 num_workers = 4
 
-train_images = glob.glob('./Dataset/TrainDataset/image/*')
-train_masks = glob.glob('./Dataset/TrainDataset/mask/*')
-test_folder = "./Dataset/TestDataset"
+# data config
+# ------------------------------------------------
+train_images = glob.glob('Dataset/TrainDataset/image/*')
+train_masks = glob.glob('Dataset/TrainDataset/mask/*')
+
+test_folder = "Dataset/TestDataset"
 test_images = glob.glob(f'{test_folder}/*/images/*')
 test_masks = glob.glob(f'{test_folder}/*/masks/*')
 
-save_path = "runs/test"
-
 image_size = 352
 
-bs = 8
-bs_val = 2
-grad_accumulate_rate = 1
+bs = 16
 
+save_path = "runs/test"
+
+# running statistic
+# ------------------------------------------------
 train_loss_meter = AverageMeter()
 iou_meter = AverageMeter()
 dice_meter = AverageMeter()
 
+# epoch config
+# ------------------------------------------------
 n_eps = 50
 save_ckpt_ep = 40
 val_ep = 40
 best = -1.
-alpha = 1
-init_lr = 1e-4
 
+# optimizer
+# ------------------------------------------------
+use_SAM = False
+optimizer = torch.optim.AdamW
+init_lr = 1e-4
+grad_accumulate_rate = 1
+optimizer_kwargs = {
+    'lr': init_lr,
+    'betas': (0.9, 0.999),
+    'weight_decay': 0.01
+}
+
+# loss config
+# ------------------------------------------------
 focal_loss = smp.losses.FocalLoss(smp.losses.BINARY_MODE)
 dice_loss = smp.losses.DiceLoss(smp.losses.BINARY_MODE)
 bce_loss = smp.losses.SoftBCEWithLogitsLoss()
-L2_loss = nn.MSELoss()
-loss_fns = [bce_loss, dice_loss, L2_loss]
-loss_weights = [0.8, 0.2]
+loss_fns = [bce_loss, dice_loss]
+loss_weights = [0.5, 0.5]
 
-train_transform_1 = A.Compose([
+# augmentation
+# ------------------------------------------------
+train_transform = A.Compose([
     A.HorizontalFlip(p=0.5),
     A.VerticalFlip(p=0.5),
-    A.ShiftScaleRotate(p=0.3, border_mode=cv2.BORDER_CONSTANT, shift_limit=0.15, scale_limit=0.11),
-])
-
-train_transform_2 = A.Compose([
     A.RandomGamma (gamma_limit=(50, 150), eps=None, always_apply=False, p=0.5),
     A.RandomBrightness(p=0.3),
     A.RGBShift(p=0.3, r_shift_limit=5, g_shift_limit=5, b_shift_limit=5),
     A.OneOf([A.Blur(), A.GaussianBlur(), A.GlassBlur(), A.MotionBlur(), A.GaussNoise(), A.Sharpen(), A.MedianBlur(), A.MultiplicativeNoise()]),
-    # A.Cutout(p=0.3, max_h_size=25, max_w_size=25, fill_value=255),
+    A.Cutout(p=0.3, max_h_size=25, max_w_size=25, fill_value=255),
+    A.ShiftScaleRotate(p=0.3, border_mode=cv2.BORDER_CONSTANT, shift_limit=0.15, scale_limit=0.11),
     A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     ToTensorV2(),
 ])
@@ -81,6 +97,8 @@ val_transform = A.Compose([
     ToTensorV2(),
 ])
 
+# deep supervision
+# ------------------------------------------------
 strategy = None # default to None
 strategy_kwargs = {
 
@@ -89,7 +107,8 @@ label_vis_kwargs = {
     'type': None
 }
 
-# pretrained = "pretrained/mit_b1_mmseg.pth"
+# model config
+# ------------------------------------------------
 pretrained = "pretrained/mit_b1_mmseg.pth"
 model_cfg = dict(
     type='SunSegmentor',
@@ -108,10 +127,9 @@ model_cfg = dict(
         drop_rate=0.0,
         attn_drop_rate=0.0,
         drop_path_rate=0.1,
-        pretrained = pretrained),
+        pretrained=pretrained),
     decode_head=dict(
         type=name_model,
-        # ops='cat',
         in_channels=[64, 128, 320, 512],
         in_index=[0, 1, 2, 3],
         channels=256,
@@ -119,16 +137,7 @@ model_cfg = dict(
         num_classes=1,
         norm_cfg=dict(type='BN', requires_grad=True),
         align_corners=False,
-        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0)),
-    # neck=dict(
-    #     type='SegformerHeadToNeck_2',
-    #     in_channels=[64, 512],
-    #     in_index=[0, 3],
-    #     channels=256,
-    #     norm_cfg=dict(type='BN', requires_grad=True),
-    #     align_corners=False,
-    #     num_classes=1
-    # )
+        loss_decode=dict(type='CrossEntropyLoss', use_sigmoid=True, loss_weight=1.0))
 )
 
 # ===============================================================================
--- train.py
+++ train.py
@@ -3,7 +3,7 @@ from tqdm import tqdm
 from tabulate import tabulate
 import logging
 import os
-from torchinfo import summary
+
 import torch
 import torch.nn.functional as F
 from torch.utils.data import DataLoader
@@ -12,28 +12,11 @@ import numpy as np
 from mmseg.models.builder import build_segmentor
 
 from mcode import ActiveDataset, get_scores, LOGGER, set_seed_everything, set_logging
+from mcode.sam import SAM
 from mcode.config import *
+from torchinfo import summary
 
-class UnNormalize(object):
-    def __init__(self, mean, std):
-        self.mean = mean
-        self.std = std
-        
-    def __call__(self, tensor):
-        """
-        Args:
-            tensor (Tensor): Tensor image of size (C, H, W) to be normalized.
-        Returns:
-            Tensor: Normalized image.
-        """
-        for t, m, s in zip(tensor, self.mean, self.std):
-            t.mul_(s).add_(m)
-            # The normalize code -> t.sub_(m).div_(s)
-        return tensor
-    
-unorm = UnNormalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225))
-
-def full_val(model):
+def full_val(model, epoch):
     print("#" * 20)
     model.eval()
     dataset_names = ['Kvasir', 'CVC-ClinicDB', 'CVC-ColonDB', 'CVC-300', 'ETIS-LaribPolypDB']
@@ -48,7 +31,7 @@ def full_val(model):
         y_test = glob.glob('{}/masks/*'.format(data_path))
         y_test.sort()
 
-        test_dataset = ActiveDataset(X_test, y_test, is_test = True, transform1=val_transform)
+        test_dataset = ActiveDataset(X_test, y_test, transform=val_transform)
         test_loader = torch.utils.data.DataLoader(
             test_dataset,
             batch_size=1,
@@ -75,8 +58,11 @@ def full_val(model):
         ious.update(mean_iou)
         dices.update(mean_dice)
         if use_wandb:
-            wandb.log({f'{dataset_name}_dice': mean_dice})
-            wandb.log({f'{dataset_name}_iou': mean_iou})
+            if use_wandb:
+                wandb.log({f'{dataset_name}_dice': mean_dice,
+                           'epoch': epoch})
+                wandb.log({f'{dataset_name}_iou': mean_iou,
+                           'epoch': epoch})
         table.append([dataset_name, mean_iou, mean_dice])
     table.append(['Total', ious.avg, dices.avg])
 
@@ -99,6 +85,7 @@ if __name__ == '__main__':
         with open(f"{save_path}/exp.log", 'w') as log_f:
             log_f.write(f"{config_data} \n")
 
+    # set_seed_everything(seed)
     if use_wandb:
         assert wandb_group is not None, "Please specify wandb group"
         wandb.login(key=wandb_key)
@@ -106,93 +93,110 @@ if __name__ == '__main__':
             project=wandb_project,
             entity=wandb_entity,
             name=wandb_name,
-            # dir=wandb_dir,
+            dir=wandb_dir,
             group=wandb_group
         )
 
-    # from pthflops import count_ops
-    # inp = torch.rand(1,3,352,352).to(device)
-    # count_ops(model, inp)
-    # dataset
-    train_dataset = ActiveDataset(
-        train_images,
-        train_masks,
-        is_test = False,
-        trainsize=image_size,
-        transform1=train_transform_1,
-        transform2=train_transform_2
-    )
-    # val_dataset = ActiveDataset(
-    #     test_images,
-    #     test_masks,
-    #     trainsize=image_size,
-    #     transform=val_transform
-    # )
-
-    set_logging("Polyp")
-    LOGGER = logging.getLogger("Polyp")
-    LOGGER.info(f"Train size: {len(train_dataset)}")
-    # LOGGER.info(f"Valid size: {len(val_dataset)}")
-
-    # dataloader
-    train_loader = DataLoader(train_dataset, batch_size=bs, num_workers=num_workers)
-    total_step = len(train_loader)
-
-        # model
+    # model
     model = build_segmentor(model_cfg)
     model.init_weights()
     model = model.to(device)
     summary(model, input_size=(1,3,352,352))
+    # dataset
+    train_dataset = ActiveDataset(
+        train_images,
+        train_masks,
+        trainsize=image_size,
+        transform=train_transform
+    )
+    val_dataset = ActiveDataset(
+        test_images,
+        test_masks,
+        trainsize=image_size,
+        transform=val_transform
+    )
+
+    set_logging("Polyp")
+    LOGGER = logging.getLogger("Polyp")
+    LOGGER.info(f"Train size: {len(train_dataset)}")
+    LOGGER.info(f"Valid size: {len(val_dataset)}")
+
+    # dataloader
+    train_loader = DataLoader(train_dataset, batch_size=bs, num_workers=num_workers, shuffle=True)
+    total_step = len(train_loader)
 
     # optimizer
-    optimizer = torch.optim.AdamW(model.parameters(), init_lr, betas=(0.9, 0.999), weight_decay=0.01)
-    lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
-                                                              T_max=len(train_loader) * n_eps,
-                                                              eta_min=init_lr / 1000)
+    if use_SAM:
+        LOGGER.warning("You're using SAM for training, training will be slower than usual")
+        optimizer = SAM(
+            model.parameters(),
+            base_optimizer=optimizer,
+            **optimizer_kwargs
+        )
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer.base_optimizer,
+                                                                  T_max=len(train_loader) * n_eps,
+                                                                  eta_min=init_lr / 1000)
+    else:
+        optimizer = optimizer(
+            model.parameters(),
+            **optimizer_kwargs
+        )
+        lr_scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer,
+                                                                  T_max=len(train_loader) * n_eps,
+                                                                  eta_min=init_lr / 1000)
+
+    # label visualize
+    label_vis_hook = LabelVis(model, save_path, strategy=strategy, **label_vis_kwargs)
+    # --- before train hooks ---
+    label_vis_hook.before_train(train_dataset)
 
     with open(f"{save_path}/exp.log", 'a') as f:
         f.write("Start Training...\n")
 
-    # set_seed_everything(seed)
     for ep in range(1, n_eps + 1):
         dice_meter.reset()
         iou_meter.reset()
         train_loss_meter.reset()
         model.train()
-        # if not os.path.exists(f"{seed}_{name_wandb}/ep_{ep}"):
-        #     os.makedirs(f"{seed}_{name_wandb}/ep_{ep}")
+
         for batch_id, sample in enumerate(tqdm(train_loader), start=1):
-            # if ep in [1, 2, 3] and batch_id < 91:
-            #     for i in range(bs):
-            #         img_demo = unorm(sample["image"][i]).permute(1, 2, 0).numpy() * 255
-            #         img_demo = cv2.cvtColor(img_demo, cv2.COLOR_BGR2RGB)
-            #         cv2.imwrite(f"{seed}_{name_wandb}/ep_{ep}/ep_1_image_id_{batch_id}_num_{i}.png", img_demo)
             if ep <= 1:
                 optimizer.param_groups[0]["lr"] = (ep * batch_id) / (1.0 * total_step) * init_lr
             else:
                 lr_scheduler.step()
-
-            n = sample["image1"].shape[0]
-            x1, x2 = sample["image1"].to(device), sample["image2"].to(device)
+            # --- data prepare ---
+            n = sample["image"].shape[0]
+            x = sample["image"].to(device)
             y = sample["mask"].to(device).to(torch.int64)
-            y_hats1 = model(x1)
-            y_hats2 = model(x2)
-            y_hats = y_hats1+y_hats2
+            # --- forward ---
+            y_hats = model(x)
+            # --- get targets ---
+            strategy_kwargs['cur_ep'] = ep # uncomment this if not strategy 2
+            targets = label_assignment(y_hats, y, strategy, **strategy_kwargs)
+            # --- loss ---
             losses = []
-            for y_hat in y_hats:
+            for i, (y_hat, y) in enumerate(zip(y_hats, targets)):
                 loss = loss_weights[0] * loss_fns[0](y_hat.squeeze(1), y.squeeze(1).float()) + \
-                       loss_weights[1] * loss_fns[1](y_hat, y)
+                    loss_weights[1] * loss_fns[1](y_hat, y)
                 losses.append(loss)
-            losses = sum(_loss for _loss in losses)
-            
-            aux_loss = loss_fns[2](y_hats1[0], y_hats2[0])
-            # print("aux: ", aux_loss.item())
-            # print("loss: ", losses.item())
-            losses = losses + alpha*aux_loss
+            losses = sum(l for l in losses)
             losses.backward()
+            # --- optimizer closure (for SAM) ---
+            def closure():
+                # --- 2nd forward ---
+                _y_hats = model(x)
+                # --- 2nd loss calc ---
+                _losses = []
+                for i, (_y_hat, _y) in enumerate(zip(_y_hats, targets)):
+                    _loss = loss_weights[0] * loss_fns[0](_y_hat.squeeze(1), _y.squeeze(1).float()) + \
+                            loss_weights[1] * loss_fns[1](_y_hat, _y)
+                    _losses.append(_loss)
+                _losses = sum(_l for _l in _losses)
+                _losses.backward()
+                return _losses
 
             if batch_id % grad_accumulate_rate == 0:
-                optimizer.step()
+                optimizer.step(closure=closure if use_SAM else None)
                 optimizer.zero_grad()
             y_hat_mask = y_hats[0].sigmoid()
             pred_mask = (y_hat_mask > 0.5).float()
@@ -204,6 +208,11 @@ if __name__ == '__main__':
             iou_meter.update(per_image_iou, n)
             dice_meter.update(dataset_iou, n)
 
+            # --- after train iter hooks ---
+            label_vis_hook.after_train_iter(batch_id, ep, strategy_kwargs)
+        # --- after train epoch hooks ---
+        label_vis_hook.after_train_epoch(ep, strategy_kwargs)
+
         LOGGER.info("EP {} TRAIN: LOSS = {}, avg_dice = {}, avg_iou = {}".format(ep, train_loss_meter.avg, dice_meter.avg,
                                                                            iou_meter.avg))
 
@@ -213,14 +222,15 @@ if __name__ == '__main__':
                                                                            iou_meter.avg))
 
         if use_wandb:
-            wandb.log({'train_dice': dice_meter.avg})
+            wandb.log({'train_dice': dice_meter.avg,
+                       'epoch': ep})
         if ep >= save_ckpt_ep:
             torch.save(model.state_dict(), f"{save_path}/checkpoints/model_{ep}.pth")
 
         if ep >= val_ep:
             # val model
             with torch.no_grad():
-                iou, dice = full_val(model)
+                iou, dice = full_val(model, ep)
                 if (dice > best):
                     torch.save(model.state_dict(), f"{save_path}/checkpoints/best.pth")
                     best = dice
