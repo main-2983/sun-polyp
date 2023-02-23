cf50576998e7b766959f9c57221f6d5d91bbc53c
main-2983 <nguyen.mai@sun-asterisk.com>
2023-02-23 09:56:54 +0700

    v2(7) and (8) - change cat to add in PFF

--- mmseg/models/decode_heads/lap_headv2.py
+++ mmseg/models/decode_heads/lap_headv2.py
@@ -572,6 +572,191 @@ class LAPHead_v2_6(BaseDecodeHead):
         return out
 
 
+# ExFuse (4 scale), (1/32 + /1/4) in skip, drop concat all
+# change cat -> add in PFF
+@HEADS.register_module()
+class LAPHead_v2_7(BaseDecodeHead):
+    def __init__(self,
+                 interpolate_mode='bilinear',
+                 **kwargs):
+        super().__init__(
+            input_transform='multiple_select',
+            **kwargs
+        )
+        self.interpolate_mode = interpolate_mode
+        num_inputs = len(self.in_channels)
+
+        assert num_inputs == len(self.in_index)
+
+        self.convs = nn.ModuleList()
+        for i in range(num_inputs):
+            self.convs.append(
+                ConvModule(
+                    in_channels=self.in_channels[i],
+                    out_channels=self.channels,
+                    kernel_size=3,
+                    padding=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg))
+
+        # feature fusion between adjacent levels
+        self.linear_projections = nn.ModuleList()
+        for i in range(num_inputs):
+            self.linear_projections.append(
+                ConvModule(
+                    in_channels=self.channels,
+                    out_channels=self.channels,
+                    kernel_size=1,
+                    stride=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg
+                )
+            )
+
+        self.se_module = SELayer(
+            channels=self.channels
+        )
+        self.fusion_conv = ConvModule(
+            in_channels=self.channels,
+            out_channels=self.channels,
+            kernel_size=1,
+            norm_cfg=self.norm_cfg)
+
+    def forward(self, inputs):
+        # inputs: 1/4, 1/8, 1/16, 1/32
+        inputs = self._transform_inputs(inputs)
+        for idx in range(len(inputs)):
+            x = inputs[idx]
+            conv = self.convs[idx]
+            inputs[idx] = resize(
+                input=conv(x),
+                size=inputs[0].shape[2:],
+                mode=self.interpolate_mode,
+                align_corners=self.align_corners
+            )
+
+        # outs: 1/32 + 1/16, 1/16 + 1/8, 1/8 + 1/4, 1/4 + 1/32
+        outs = []
+        for idx in range(len(inputs) - 1, -1, -1):
+            linear_prj = self.linear_projections[idx]
+            # cat first 2 from inputs
+            if idx == len(inputs) - 1:
+                x1 = inputs[idx]
+                x2 = inputs[idx - 1]
+            # if not first 2 then cat from prev outs and inputs
+            else:
+                x1 = _out
+                x2 = inputs[idx - 1]
+            x = x1 + x2
+            _out = linear_prj(x)
+            outs.append(_out)
+
+        out = outs[-1]
+        out = self.se_module(out)
+        out = self.fusion_conv(out)
+        # perform identity mapping
+        out = outs[-1] + out
+
+        out = self.cls_seg(out)
+
+        return out
+
+
+# ExFuse (3 scale), (1/32 + /1/4) in skip, drop concat all
+# change cat -> add in PFF
+@HEADS.register_module()
+class LAPHead_v2_8(BaseDecodeHead):
+    def __init__(self,
+                 interpolate_mode='bilinear',
+                 **kwargs):
+        super().__init__(
+            input_transform='multiple_select',
+            **kwargs
+        )
+        self.interpolate_mode = interpolate_mode
+        num_inputs = len(self.in_channels)
+
+        assert num_inputs == len(self.in_index)
+
+        self.convs = nn.ModuleList()
+        for i in range(num_inputs):
+            self.convs.append(
+                ConvModule(
+                    in_channels=self.in_channels[i],
+                    out_channels=self.channels,
+                    kernel_size=3,
+                    padding=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg))
+
+        # feature fusion between adjacent levels
+        self.linear_projections = nn.ModuleList()
+        for i in range(num_inputs - 1):
+            self.linear_projections.append(
+                ConvModule(
+                    in_channels=self.channels,
+                    out_channels=self.channels,
+                    kernel_size=1,
+                    stride=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg
+                )
+            )
+
+        self.se_module = SELayer(
+            channels=self.channels
+        )
+        self.fusion_conv = ConvModule(
+            in_channels=self.channels,
+            out_channels=self.channels,
+            kernel_size=1,
+            norm_cfg=self.norm_cfg)
+
+    def forward(self, inputs):
+        # inputs: 1/4, 1/8, 1/16, 1/32
+        inputs = self._transform_inputs(inputs)
+        for idx in range(len(inputs)):
+            x = inputs[idx]
+            conv = self.convs[idx]
+            inputs[idx] = resize(
+                input=conv(x),
+                size=inputs[0].shape[2:],
+                mode=self.interpolate_mode,
+                align_corners=self.align_corners
+            )
+
+        # new concatenation order:
+        # 1/16 + 1/8 -> 1/8 + 1/4 -> 1/4 + 1/32
+        # rearrange inputs to 1/32, 1/4, 1/8, 1/16
+        inputs = inputs[-1:] + inputs[0:-1]
+        # 1/16, 1/16 + 1/8, 1/8 + 1/4, 1/4 + 1/32
+        outs = [inputs[-1]]
+        # idx: 3, 2, 1
+        for idx in range(len(inputs) - 1, 0, -1):
+            linear_prj = self.linear_projections[idx - 1]
+            # cat first 2 from inputs
+            if idx == len(inputs) - 1:
+                x1 = inputs[idx]
+                x2 = inputs[idx - 1]
+            # if not first 2 then cat from prev outs and inputs
+            else:
+                x1 = _out
+                x2 = inputs[idx - 1]
+            x = x1 + x2
+            _out = linear_prj(x)
+            outs.append(_out)
+
+        out = outs[-1]
+        out = self.se_module(out)
+        out = self.fusion_conv(out)
+        # perform identity mapping
+        out = outs[-1] + out
+
+        out = self.cls_seg(out)
+
+        return out
+
+
 # Change cat -> add in PFF
 @HEADS.register_module()
 class LAPHead_v2_9(BaseDecodeHead):
