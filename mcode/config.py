5e56172523958764ebca06ce8910dc616e66e741
Bui Quang Manh <bui.quang.manh@sun-asterisk.com>
2023-03-14 07:33:54 +0000

    init

--- mcode/config.py
+++ mcode/config.py
@@ -60,7 +60,7 @@ loss_fns = [bce_loss, dice_loss]
 loss_weights = [0.5, 0.5]
 
 transform = A.Compose([
-    # A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
+    A.Normalize(mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)),
     ToTensorV2(),
 ])
 
