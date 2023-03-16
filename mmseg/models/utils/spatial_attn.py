1cc7bb4d601ef80032d7bff0d6fe7366add23706
main-2983 <nguyen.mai@sun-asterisk.com>
2023-03-16 15:02:52 +0700

    Add AvgSpatialAttn

--- mmseg/models/utils/spatial_attn.py
+++ mmseg/models/utils/spatial_attn.py
@@ -9,6 +9,15 @@ __all__ = [
 ]
 
 
+class AvgSpatial(nn.Module):
+    def __init__(self, dim=1):
+        super(AvgSpatial, self).__init__()
+        self.dim = dim
+
+    def forward(self, x):
+        return torch.mean(x, dim=self.dim, keepdim=True)
+
+
 class AvgSpatialAttn(nn.Module):
     """
     This module use spatial mean to squeeze the feature maps into 2D plane of shape (B, 1, H, W)
@@ -20,19 +29,22 @@ class AvgSpatialAttn(nn.Module):
                  kernel_size=7,
                  norm_cfg=None,
                  act_cfg=dict(
-                     type='Sigmoid'
+                     type='ReLU'
                  )):
         super(AvgSpatialAttn, self).__init__()
+        self.avg = AvgSpatial()
         self.conv = ConvModule(
             in_channels=1,
             out_channels=1,
             kernel_size=kernel_size,
             norm_cfg=norm_cfg,
-            act_cfg=act_cfg
+            act_cfg=act_cfg,
+            padding=kernel_size//2
         )
         self.channels = channels
 
     def forward(self, x):
-        weight:torch.Tensor = torch.mean(x, dim=1, keepdim=True)
+        weight = self.avg(x)
+        weight = self.conv(weight)
         weight = weight.expand(-1, self.channels, -1, -1)
         return weight * x
