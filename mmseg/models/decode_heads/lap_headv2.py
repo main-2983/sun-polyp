ef380a0fbb3c64eaf410d07bb315f0c8802cb875
main-2983 <nguyen.mai@sun-asterisk.com>
2023-03-22 09:52:31 +0700

    v2(34) - add DWConv to 1st stage

--- mmseg/models/decode_heads/lap_headv2.py
+++ mmseg/models/decode_heads/lap_headv2.py
@@ -1,7 +1,7 @@
 import torch
 import torch.nn as nn
 
-from mmcv.cnn import ConvModule
+from mmcv.cnn import ConvModule, DepthwiseSeparableConvModule
 
 from mmseg.models.builder import HEADS
 from mmseg.models.decode_heads.decode_head import BaseDecodeHead
@@ -1934,6 +1934,100 @@ class LAPHead_v2_33(BaseDecodeHead):
                 x1 = out
                 x2 = inputs[idx - 1]
             x = x1 + x2
+            out = linear_prj(x)
+            outs.append(out)
+
+        out = outs[-1]
+        out = self.se_module(out)
+        out = self.cls_seg(out)
+
+        return out
+
+
+# ExFuse, remove cat all, cat -> add and scale, add SE
+# Add DWConv to early stage
+@HEADS.register_module()
+class LAPHead_v2_34(BaseDecodeHead):
+    def __init__(self,
+                 interpolate_mode='bilinear',
+                 scale_init_vals=[1.0, 1.0, 1.0, 1.0],
+                 dw_kernel_size=3,
+                 **kwargs):
+        super().__init__(input_transform='multiple_select', **kwargs)
+
+        self.interpolate_mode = interpolate_mode
+        num_inputs = len(self.in_channels)
+
+        assert num_inputs == len(self.in_index)
+
+        self.convs = nn.ModuleList()
+        for i in range(num_inputs):
+            if i < 3: # except first stage
+                self.convs.append(
+                    ConvModule(
+                        in_channels=self.in_channels[i],
+                        out_channels=self.channels,
+                        kernel_size=1,
+                        norm_cfg=self.norm_cfg,
+                        act_cfg=self.act_cfg))
+            else:
+                self.convs.append(
+                    DepthwiseSeparableConvModule(
+                        in_channels=self.in_channels[i],
+                        out_channels=self.channels,
+                        kernel_size=dw_kernel_size,
+                        padding=dw_kernel_size//2,
+                        norm_cfg=self.norm_cfg,
+                        act_cfg=self.act_cfg
+                    )
+                )
+
+        self.linear_prj = nn.ModuleList()
+        self.scales = nn.ModuleList()
+        for i in range(num_inputs):
+            self.linear_prj.append(
+                ConvModule(
+                    in_channels=self.channels,
+                    out_channels=self.channels,
+                    kernel_size=1,
+                    norm_cfg=self.norm_cfg,
+                    act_cfg=self.act_cfg
+                )
+            )
+            self.scales.append(
+                Scale(
+                    channels=self.channels,
+                    init_val=scale_init_vals[i]
+                )
+            )
+        self.se_module = SELayer(
+            channels=self.channels,
+            ratio=8
+        )
+
+    def forward(self, inputs):
+        # Receive 4 stage backbone feature map: 1/4, 1/8, 1/16, 1/32
+        inputs = self._transform_inputs(inputs)
+        for idx in range(len(inputs)):
+            x = inputs[idx]
+            conv = self.convs[idx]
+            inputs[idx] = resize(
+                input=conv(x),
+                size=inputs[0].shape[2:],
+                mode=self.interpolate_mode,
+                align_corners=self.align_corners)
+
+        outs = []
+        for idx in range(len(inputs) -1, -1, -1):
+            linear_prj = self.linear_prj[idx]
+            scale = self.scales[idx]
+            if idx == len(inputs) - 1:
+                x1 = inputs[idx]
+                x2 = inputs[idx - 1]
+            else:
+                x1 = out
+                x2 = inputs[idx - 1]
+            x = scale(x1) + x2
             out = linear_prj(x)
             outs.append(out)
 
